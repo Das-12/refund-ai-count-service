82fe340a3aab1d376204e6ea2846c9da11645845
Vishnuk2259 <vishnukottapattu@gmail.com>
2025-01-08 10:52:49 +0530

    changed the db config

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 import os
 
 # MONGO_DETAILS = "mongodb+srv://muhammedarshadm:QgZEv11DThwYkC1y@cluster0.mfwgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
-MONGO_DETAILS = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_DETAILS = os.getenv("MONGO_URI", "mongodb+srv://localhost:27017")
 client = AsyncIOMotorClient(MONGO_DETAILS)
 database = client.logs
 counts_collection = database.get_collection("counts")
